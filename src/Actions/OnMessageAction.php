3b829e9acb5bb3753da3a6222ae3def827edac4c
yumerov <yumerov.levent@gmail.com>
2023-03-25 19:29:41 +0200

    Changed env loading

--- src/Actions/OnMessageAction.php
+++ src/Actions/OnMessageAction.php
@@ -5,16 +5,26 @@ namespace Yumerov\MaxiBot\Actions;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Psr\Log\LoggerInterface;
+use Yumerov\MaxiBot\Firewalls\AllowedServerFirewall;
+use Yumerov\MaxiBot\Firewalls\NotMeFirewall;
 
 class OnMessageAction
 {
 
-    public function __construct(private readonly LoggerInterface $logger)
+    public function __construct(private readonly LoggerInterface $logger, private readonly array $env)
     {
     }
 
     public function __invoke(Message $message, Discord $discord): void
     {
+        if (!(new NotMeFirewall($discord, $message, $this->logger, $this->env))->allow()) {
+            return;
+        }
+
+        if (!(new AllowedServerFirewall($message, $this->logger, $this->env))->allow()) {
+            return;
+        }
+
         if ($message->author->bot) {
             $this->logger->debug('The author is an bot');
             return;
