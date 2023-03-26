53f523d1e0fbb45870bfe35103341557fa92b842
yumerov <yumerov.levent@gmail.com>
2023-03-26 02:03:13 +0200

    NotMe firewall containing more checks

--- tests/Firewalls/NotMeFirewallTest.php
+++ tests/Firewalls/NotMeFirewallTest.php
@@ -34,7 +34,7 @@ class NotMeFirewallTest extends TestCase
     public function test_null_author(): void
     {
         // Act && Assert
-        $this->assertTrue($this->firewall->allow());
+        $this->assertFalse($this->firewall->allow());
     }
 
     public function test_author_is_this(): void
@@ -47,11 +47,57 @@ class NotMeFirewallTest extends TestCase
         $this->assertFalse($this->firewall->allow());
     }
 
-    public function test_author_not_is_this(): void
+    public function test_zero_mentions(): void
     {
         // Arrange
         $this->message->author = new User('0');
         $this->discord->user = new User('1');
+
+        // Act && Assert
+        $this->assertFalse($this->firewall->allow());
+    }
+
+    public function test_two_mentions(): void
+    {
+        // Arrange
+        $this->message->author = new User('0');
+        $this->message->mentions->push(new User('2'), new User('3'));
+        $this->discord->user = new User('1');
+
+        // Act && Assert
+        $this->assertFalse($this->firewall->allow());
+    }
+
+    public function test_mention_is_not_the_bot(): void
+    {
+        // Arrange
+        $this->message->author = new User('0');
+        $this->message->mentions->push(new User('2'));
+        $this->discord->user = new User('1');
+
+        // Act && Assert
+        $this->assertFalse($this->firewall->allow());
+    }
+
+    public function test_bot_mentions_itself(): void
+    {
+        // Arrange
+        $bot = new User('1');
+        $this->message->author = $bot;
+        $this->message->mentions->push($bot);
+        $this->discord->user = $bot;
+
+        // Act && Assert
+        $this->assertFalse($this->firewall->allow());
+    }
+
+    public function test_user_mentions_the_bot(): void
+    {
+        // Arrange
+        $bot = new User('1');
+        $this->message->author = new User('0');
+        $this->message->mentions->push($bot);
+        $this->discord->user = $bot;
 
         // Act && Assert
         $this->assertTrue($this->firewall->allow());
