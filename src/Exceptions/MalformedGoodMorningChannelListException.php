c7be5b267789b384a40e5b36d2a058bdfcbaaa5a
yumerov <yumerov.levent@gmail.com>
2023-04-01 19:23:52 +0300

    Reformatted long line

--- src/Exceptions/MalformedGoodMorningChannelListException.php
+++ src/Exceptions/MalformedGoodMorningChannelListException.php
@@ -7,8 +7,11 @@ use Throwable;
 class MalformedGoodMorningChannelListException extends Exception
 {
 
-    public function __construct(string $message = "Malformed good morning channel list", int $code = 0, ?Throwable $previous = null)
-    {
+    public function __construct(
+        string $message = "Malformed good morning channel list",
+        int $code = 0,
+        ?Throwable $previous = null
+    ) {
         parent::__construct($message, $code, $previous);
     }
 }
