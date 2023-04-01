7970f82979f7ad78e53d25f20eb6fe80b85b4ee4
yumerov <yumerov.levent@gmail.com>
2023-04-01 19:22:08 +0300

    Printing output if tests fail

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,1 +1,8 @@
-phpunit --coverage-text | grep "Lines" | grep -v "Methods" | sed "s/\sLines\://g" | sed "s/%.*//g" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | awk '{if ($0 > 80) exit 0; else print "Test coverage " $0 "% under 80%!"; exit 1;}'
+output=$(phpunit --coverage-text)
+
+if [ $? -eq 0 ]; then
+    echo $output | grep "Lines" | grep -v "Methods" | sed "s/\sLines\://g" | sed "s/%.*//g" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | awk '{if ($0 > 50) exit 0; else print "Test coverage " $0 "% under 80%!"; exit 1;}'
+else
+  echo $output;
+  exit 0;
+fi
