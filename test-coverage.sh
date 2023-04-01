0b370d595033eafebca5c512790fb5fcc9a3fe88
yumerov <yumerov.levent@gmail.com>
2023-04-01 11:28:24 +0300

    Verbose test coverage script

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,1 +1,1 @@
-phpunit --coverage-text | grep "Lines" | grep -v "Methods" | sed "s/\sLines\://g" | sed "s/%.*//g" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | awk '{if ($0 > 80) exit 0; else exit 1;}' || echo "Test coverage under 80%" && exit 1;
+phpunit --coverage-text | grep "Lines" | grep -v "Methods" | sed "s/\sLines\://g" | sed "s/%.*//g" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' | awk '{if ($0 > 80) exit 0; else print "Test coverage " $0 "% under 80%!"; exit 1;}'
