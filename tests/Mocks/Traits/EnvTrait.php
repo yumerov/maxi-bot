676c246a5f3c2b9e27697dc003517a111570d1be
yumerov <yumerov.levent@gmail.com>
2023-04-01 14:13:35 +0300

    test env trait refactored

---
+++ tests/Mocks/Traits/EnvTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Yumerov\MaxiBot\Mocks\Traits;
+
+use Yumerov\MaxiBot\DTO\EnvDTO;
+
+trait EnvTrait
+{
+
+    private array $envData = [
+        'DISCORD_TOKEN' => '0xtoken',
+        'GOOD_MORNING_CHANNELS' => '["0"]',
+        'MAINTAINER' => '1',
+        'ALLOWED_SERVERS' => '["2"]',
+        'MAINTAINER_ONLY_MODE' => 'true'
+    ];
+
+    protected function createEnvDTO(string $goodMorningChannels = '[]'): EnvDTO
+    {
+        return new EnvDTO([
+            'DISCORD_TOKEN' => '0xtoken',
+            'GOOD_MORNING_CHANNELS' => $goodMorningChannels,
+            'MAINTAINER' => '1',
+            'ALLOWED_SERVERS' => '["2"]',
+            'MAINTAINER_ONLY_MODE' => 'true',
+        ]);
+    }
+
+    protected function unsetEnv(array $keys): void
+    {
+        foreach ($keys as $key)
+        {
+            unset($_ENV[$key]);
+        }
+    }
+}
