8b354e0c403b5b5aeb85d161969794261af9a0f2
yumerov <yumerov.levent@gmail.com>
2023-04-13 17:41:23 +0300

    [WIP] Sample quotes

---
+++ data/quotes.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'You can’t stop things like Bitcoin. It will be everywhere and the world will have to readjust. World governments will have to readjust.',
+    'Bitcoin is the currency of resistance.',
+    'I think anybody who is interested in keeping their money safe from the criminal banking system would want gold, silver, and Bitcoin.',
+];
