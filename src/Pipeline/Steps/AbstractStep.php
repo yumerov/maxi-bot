5ae777a7a7b7331941801173bf28f3b389c9976f
yumerov <yumerov.levent@gmail.com>
2023-04-16 13:13:04 +0300

    [WIP] HasDiscordTrait

--- src/Pipeline/Steps/AbstractStep.php
+++ src/Pipeline/Steps/AbstractStep.php
@@ -5,21 +5,17 @@ namespace Yumerov\MaxiBot\Pipeline\Steps;
 use Discord\Discord;
 use Discord\Parts\Channel\Message;
 use Psr\Log\LoggerInterface;
+use Yumerov\MaxiBot\Traits\HasDiscordTrait;
 
 abstract class AbstractStep implements StepInterface
 {
+    use HasDiscordTrait;
 
     protected Discord $discord;
     protected Message $message;
 
     public function __construct(protected readonly LoggerInterface $logger)
     {
-    }
-
-    public function setDiscord(Discord $discord): static
-    {
-        $this->discord = $discord;
-        return $this;
     }
 
     public function setMessage(Message $message): static
